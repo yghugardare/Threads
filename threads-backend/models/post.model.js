a7c63b2db32e5d4645405ed835f0dfe1d412b71d
Yash Ghugardare <yghugardare6@gmail.com>
2024-07-24 15:17:02 +0530

    feat: created post model

---
+++ threads-backend/models/post.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+const postSchema = new mongoose.Schema({
+    postedBy : {
+        // user id , basically a foreign key
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    text : {
+        type : String,
+        maxLength : 500,
+    },
+    img : {
+        type : String,
+    },
+    likes : {
+        // array of user ids
+        type : [mongoose.Schema.Types.ObjectId],
+        default : [],
+        ref : "User"
+    },
+    replies : [{
+        userid : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        userProfilePic: {
+            type: String,
+        },
+        username: {
+            type: String,
+        },
+    }]
+},{timestamps : true})
+
+export const Post = mongoose.model("Post",postSchema)
--- threads-backend/models/user.model.js
+++ threads-backend/models/user.model.js
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
     password : {
         type :String,
         required : [true,"Password of the user is required"],
-        unique : true
+        unique : true,
+        minLength : 6
     },
     followers : {
         type : [String],
