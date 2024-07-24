6d2d9f5cf888b8504b1048cb989e2569334c84e6
Yash Ghugardare <yghugardare6@gmail.com>
2024-07-24 14:49:46 +0530

    feat: user model added for the app

---
+++ threads-backend/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose"
+const userSchema = new mongoose.Schema({
+    name : {
+        type :String,
+        required : [true,"Name of the user is required"]
+    },
+    username : {
+        type :String,
+        required : [true,"Username of the user is required"],
+        unique : true
+    },
+    email : {
+        type :String,
+        required : [true,"email of the user is required"],
+        unique : true
+    },
+    password : {
+        type :String,
+        required : [true,"Password of the user is required"],
+        unique : true
+    },
+    followers : {
+        type : [String],
+        default : []
+    },
+    following : {
+        type : [String],
+        default : []
+    },
+    profilePic : {
+        type : String,
+        default : ""
+    },
+    bio : {
+        type: String,
+        default : ""
+    },
+    isFrozen : {
+        type : Boolean,
+        default : false
+    }
+
+}, {timestamps : true})
+
+export const User = mongoose.model("User",userSchema)
+
+// User -> users
