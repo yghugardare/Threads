5441e7194765795439532880d0922fe93b31202c
Yash Ghugardare <yghugardare6@gmail.com>
2024-07-24 15:32:35 +0530

    feat:added a basic middleware configuration for parsing

--- threads-backend/server.js
+++ threads-backend/server.js
@@ -1,9 +1,20 @@
 import express from "express"
 import dotenv from  "dotenv"
 import { connectDb } from "./db/connectDb.js"
+import cookieParser from "cookie-parser"
 dotenv.config()
 connectDb()
 const app = express()
+
+// global middlewares
+// parses the JSON payload of incoming requests and makes it accessible in req.body. 
+app.use(express.json())
+// built-in middleware function in Express.js that parses incoming requests with URL-encoded payloads 
+// extends:true => Supports nested objects and arrays.
+app.use(express.urlencoded({extended:true}))
+// parses cookies attached to the incoming requests and makes them accessible in the req.cookies object.
+app.use(cookieParser())
+
 
 const PORT = process.env.PORT || 5000
 
