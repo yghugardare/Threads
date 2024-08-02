eb8cd8dbd615ddb537612a3104f9f391739eef7f
Yash Ghugardare <yghugardare6@gmail.com>
2024-08-02 23:38:08 +0530

    feat: Homepage and user feed posts added

---
+++ threads-ui/src/components/Post.jsx
@@ -0,0 +1,128 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react"
+import useShowToast from "../hooks/useShowToast";
+import {Link, useNavigate} from "react-router-dom";
+import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
+import Actions from "./Actions";
+import { formatDistanceToNow } from "date-fns";
+
+function Post({post,postedBy}) {
+    const [liked, setLiked] = useState(false);
+    const [user, setUser] = useState(null);
+    const showToast = useShowToast();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const getUser = async () => {
+           try{
+
+            const res = await fetch(`/api/users/profile/${postedBy}`);
+            const data = await res.json();
+            if(data.error){
+              showToast("Error",data.error,"error")
+              return;
+            }
+            setUser(data);
+
+           }catch(error){
+            showToast("Error",error,"error");
+            setUser(null)
+           }
+           
+        }
+        getUser();
+    },[postedBy,showToast]);
+  if(!user) return null;
+  return (
+    <Link to={`/${user.username}/post/${post._id}`}>
+    <Flex gap={3} mb={4} py={5}>
+      <Flex flexDirection={"column"} alignItems={"center"}>
+        <Avatar
+          size='md'
+          name={user.name}
+          src={user?.profilePic}
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(`/${user.username}`);
+          }}
+        />
+        <Box w='1px' h={"full"} bg='gray.light' my={2}></Box>
+        <Box position={"relative"} w={"full"}>
+          {post.replies.length === 0 && <Text textAlign={"center"}>🥱</Text>}
+          {post.replies[0] && (
+            <Avatar
+              size='xs'
+              name='John doe'
+              src={post.replies[0].userProfilePic}
+              position={"absolute"}
+              top={"0px"}
+              left='15px'
+              padding={"2px"}
+            />
+          )}
+
+          {post.replies[1] && (
+            <Avatar
+              size='xs'
+              name='John doe'
+              src={post.replies[1].userProfilePic}
+              position={"absolute"}
+              bottom={"0px"}
+              right='-5px'
+              padding={"2px"}
+            />
+          )}
+
+          {post.replies[2] && (
+            <Avatar
+              size='xs'
+              name='John doe'
+              src={post.replies[2].userProfilePic}
+              position={"absolute"}
+              bottom={"0px"}
+              left='4px'
+              padding={"2px"}
+            />
+          )}
+        </Box>
+      </Flex>
+      <Flex flex={1} flexDirection={"column"} gap={2}>
+        <Flex justifyContent={"space-between"} w={"full"}>
+          <Flex w={"full"} alignItems={"center"}>
+            <Text
+              fontSize={"sm"}
+              fontWeight={"bold"}
+              onClick={(e) => {
+                e.preventDefault();
+                navigate(`/${user.username}`);
+              }}
+            >
+              {user?.username}
+            </Text>
+            <Image src='/verified.png' w={4} h={4} ml={1} />
+          </Flex>
+          <Flex gap={4} alignItems={"center"}>
+            <Text fontSize={"xs"} width={36} textAlign={"right"} color={"gray.light"}>
+              {formatDistanceToNow(new Date(post.createdAt))} ago
+            </Text>
+
+            {/* {currentUser?._id === user._id && <DeleteIcon size={20} onClick={handleDeletePost} />} */}
+          </Flex>
+        </Flex>
+
+        <Text fontSize={"sm"}>{post.text}</Text>
+        {post.img && (
+          <Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
+            <Image src={post.img} w={"full"} />
+          </Box>
+        )}
+
+        <Flex gap={3} my={1}>
+          <Actions liked={liked} setLiked={setLiked} />
+        </Flex>
+      </Flex>
+    </Flex>
+  </Link>
+  )
+}
+
+export default Post
--- threads-ui/src/pages/HomePage.jsx
+++ threads-ui/src/pages/HomePage.jsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
+import Post from "../components/Post";
 
 function HomePage() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const showToast = useShowToast();
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/posts/feed`);
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error, "erroor");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast]);
   return (
-    <div>HomePage</div>
-  )
+    <>
+      {!loading && posts.length === 0 && (
+        <h1>Follow some user to see the feed at the Home page!!</h1>
+      )}
+      {loading && (
+        <Flex justify={"center"}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+      {
+        posts.map(post => (
+          <Post key={post._id} post={post} postedBy={post.postedBy}/>
+        ))
+      }
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
