d6183a7d3e1dc8863ec56759cf6b1343c4692198
Yash Ghugardare <yghugardare6@gmail.com>
2024-08-13 22:29:17 +0530

    feat: get messages controller added
    used to get messages of the other person [whom i am chatting with] between him and me

--- threads-backend/controller/message.controller.js
+++ threads-backend/controller/message.controller.js
@@ -3,43 +3,58 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import Conversation from "../models/conversation.model.js";
 
-const sendMessage = async (req,res) => {
-    try {
-        const {recipientId,message} = req.body;
-        const senderId =req.user._id;
-        let conversation = await Conversation.findOne({
-            participants : {$all : [senderId,recipientId]},
-        })
-        if(!conversation){
-            conversation = new Conversation({
-                participants : [senderId,recipientId],
-                lastMessage : {
-                    text : message,
-                    sender : senderId,
-                },
-
-            })
-            await conversation.save();
-        }
-        const newMessage = new Message({
-            conversationId : conversation._id,
-            sender : senderId,
-            text : message
-        });
-        await Promise.all([
-            newMessage.save(),
-            conversation.updateOne({
-                lastMessage : {
-                    text : message,
-                    sender : senderId
-                }
-            })
-        ])
-        res.status(201).json(newMessage);
-    } catch (error) {
-        res.status(500).json({error : error.message});
+const sendMessage = async (req, res) => {
+  try {
+    const { recipientId, message } = req.body;
+    const senderId = req.user._id;
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, recipientId] },
+    });
+    if (!conversation) {
+      conversation = new Conversation({
+        participants: [senderId, recipientId],
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      });
+      await conversation.save();
     }
-}
-export {
-    sendMessage
-}
+    const newMessage = new Message({
+      conversationId: conversation._id,
+      sender: senderId,
+      text: message,
+    });
+    await Promise.all([
+      newMessage.save(),
+      conversation.updateOne({
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      }),
+    ]);
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+const getMessage = async (req, res) => {
+  const { otherUserId } = req.params;
+  const userId = req.user._id;
+  try {
+    let conversation = await Conversation.findOne({
+      participants: { $all: [userId, otherUserId] },
+    });
+    if (!conversation) {
+      return res.status(404).json({ error: "Conversation Not Found!" });
+    }
+    const messages = await Message.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 });
+    res.status(200).json(messages);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+export { sendMessage,getMessage };
--- threads-backend/routes/message.route.js
+++ threads-backend/routes/message.route.js
@@ -1,8 +1,10 @@
-import express from "express"
+import express from "express";
 import { protectRoute } from "../middlewares/protectedRoute.js";
-import { sendMessage } from "../controller/message.controller.js";
+import { getMessage, sendMessage } from "../controller/message.controller.js";
 const messageRouter = express.Router();
 
-messageRouter.post("/",protectRoute,sendMessage)
+messageRouter.post("/", protectRoute, sendMessage);
+// get messages of the other person [whom i am chatting with] between him and me
+messageRouter.get("/:otherUserId", protectRoute, getMessage);
 
-export {messageRouter}
+export { messageRouter };
